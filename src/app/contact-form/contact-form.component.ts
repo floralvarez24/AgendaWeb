dfe3be6569d592bad6d3548407cf8185c6907eba
floralvarez24 <floralvarez04@hotmail.com>
2025-09-22 21:13:47 -0300

    bugs corregidos

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,19 +1,16 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
-import {RouterModule, Router, ActivatedRoute } from '@angular/router';
-import {ContactService} from '../services/contact.service';
-import { OnInit } from '@angular/core';
+import { RouterModule, Router, ActivatedRoute } from '@angular/router';
+import { ContactService } from '../services/contact.service';
 import { Contact } from '../model/contact.interface';
 import { Observable } from 'rxjs';
-
-
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
   imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.css'
+  styleUrls: ['./contact-form.component.css'] // ojo: style**s**Urls en plural
 })
 export default class ContactFormComponent implements OnInit {
 
@@ -22,55 +19,54 @@ export default class ContactFormComponent implements OnInit {
   private router = inject(Router);
   private route = inject(ActivatedRoute);
 
-  form?: FormGroup;
+  form!: FormGroup;
   contact?: Contact;
   errors: string[] = [];
 
   ngOnInit(): void {
-   const id = this. route.snapshot.paramMap.get('id');
-   
-   if (id){
-    this.contactService.get(parseInt(id)).subscribe(contact => {
-      this.contact = contact;
-      this.form = this.fb.group({
-        name: [contact.name, Validators.required],
-        email: [contact.email, Validators.required, Validators.email]
-      });
-    });
-    
-  } else {
-    this.form = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', Validators.required, Validators.email]
-    });
-  }
-}
+    const id = this.route.snapshot.paramMap.get('id');
 
-  save (){
-    if (this.form?.invalid){
+    if (id) {
+      this.contactService.get(+id).subscribe(contact => {
+        this.contact = contact;
+        this.form = this.fb.group({
+          name: [contact.name, [Validators.required]],
+          email: [contact.email, [Validators.required, Validators.email]]
+        });
+      });
+    } else {
+      this.form = this.fb.group({
+        name: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]]
+      });
+    }
+  }
+
+  save(): void {
+    if (this.form?.invalid) {
       this.form.markAllAsTouched();
       return;
     }
 
-    const contactForm = this.form!.value;
+    const contactForm = this.form.value;
     let request: Observable<Contact>;
 
-    if (this.contact){
+    if (this.contact) {
       request = this.contactService.update(this.contact.id, contactForm);
-    }else {
+    } else {
       request = this.contactService.create(contactForm);
     }
 
-    request
-    .subscribe({
+    request.subscribe({
       next: () => {
-      this.errors = [];
-      this.router.navigate(['/']);
-     },
-     error: response => {
+        this.errors = [];
+        this.router.navigate(['/']);
+      },
+      error: response => {
         this.errors = response.error.errors;
       }
-    })
+    });
   }
 }
+
 
